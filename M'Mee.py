1087a43a49c17d187b85b47f13365378e27c691c
Sudarat1003 <52315113+Sudarat1003@users.noreply.github.com>
2019-07-05 14:06:23 +0700

    Add files via upload
    โปรแกรมแรก

---
+++ M'Mee.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[23]:
+
+
+print(type("10"))
+
+
+# In[15]:
+
+
+print(type("Hello"))
+
+
+# In[16]:
+
+
+print(type(True))
+
+
+# In[18]:
+
+
+print(type(10.3))
+
+
+# In[17]:
+
+
+print(type(1j))
+
+
+# In[24]:
+
+
+print((10+3.5))
+
+
+# In[ ]:
+
+
+
+
