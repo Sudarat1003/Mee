e6160ee614e19feb656dd4826808dcc851fc3633
Sudarat1003 <52315113+Sudarat1003@users.noreply.github.com>
2019-07-12 17:14:33 +0700

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x=10
+y=5
+z=x+y
+print (z)
+
+
+# In[5]:
+
+
+money=150
+incomePD=300
+cosPD=120
+result=150+300*30-120*30
+print(result)
+
+
+# In[14]:
+
+
+x=True
+y=False
+z=x or y
+a=10
+b=5
+c=a>b
+c=5
+prin(c)
+
+
+# In[11]:
+
+
+a=10
+b=5
+c=a+b
+b=10
+print(b)
+
+
+# In[16]:
+
+
+a=10
+b=12
+c=a+b
+a=a+5
+print(a)
+
+
+# In[22]:
+
+
+mony=1000
+mony=mony-5
+mony=mony-20
+print(mony)
+
+
+# In[23]:
+
+
+money=1000
+money+=money+50
+money-=money-100
+money*=money*10
+money/=money/2
+print(money)
+
+
+# In[24]:
+
+
+a=12.0
+print(int(a))
+print(a)
+print(str(a))
+print(type(int(a)))
+print(type(a))
+print(type(str(a)))
+
+
+# In[ ]:
+
+
+number1:input("number:")
+number2:input("number:")
+print("number1+number2",int(num1)+int(num2))
+
+
+# In[ ]:
+
+
+name:
+How old are you:
+Hello      is       Year Old:
+print("name,"+",How old are you,''+",Hello is year old."='',int(name)+int(How old are you)+(Hello is year old.))
+     
+
+
+# In[ ]:
+
+
+price=int(input())
+print(type(price))
+
+
+# In[ ]:
+
+
+
+
