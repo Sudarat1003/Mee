1a20678be7cb9beb6999cdb68317be101b6a1ed0
Sudarat1003 <52315113+Sudarat1003@users.noreply.github.com>
2019-07-19 16:47:18 +0700

    Add files via upload

---
+++ 190762.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+if True:
+    print("Hello Condition1")
+    print("Hello Condition2")
+
+
+# In[13]:
+
+
+if False:
+    print("Hello Condition1")
+    print("Hello Condition2")    
+
+
+# In[12]:
+
+
+money=1000
+if money>300:
+    print("Go to KFC")
+else:
+    print("SO Sad !")
+print("Bye Bye !")
+    
+   
+
+
+# In[30]:
+
+
+usrenameinput=input("usre name:")
+passwordinput=input("password:")
+if usrenameinput=="admin" and passwordinput =="1234":
+    print("Done!")
+else:
+    print("Erro")
+         
+
+
+# In[34]:
+
+
+if False:
+    print("frist Condition")
+    print("frist Condition")
+    print("frist Condition")
+    print("frist Condition")
+    print("frist Condition")
+elif False:
+    print("Second Condition")
+elif True:
+    print("Third Condition")
+else:
+     print("Else Condition")
+
+
+# In[ ]:
+
+
+print(".......Menu........")
+print("1Strawberry     =80THB")
+print("2Vanila            =50THB")
+print("3Chocolate       =40THB")
+print("4Lemon           =90THB")
+print("5Milk               =10THB")
+CS=input("Do you want?")
+
+
+# In[ ]:
+
+
+if False:
+    ("Hello wecome !")
+if True:
+    print("Yo ! mister A")
+
+
+# In[ ]:
+
+
+
+
