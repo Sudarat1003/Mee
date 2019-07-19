9a830239ccbfe6e972326d2c4b4c64476e821bae
Sudarat1003 <52315113+Sudarat1003@users.noreply.github.com>
2019-07-19 14:01:03 +0700

    Add files via upload

---
+++ Exercise5_1_Sudarat O.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+a=10
+b=2
+c=10+2
+d=10-2
+e=10*2
+f=10/2
+print("10+2=",int(c))
+print("10-2=",int(d))
+print("10*2=",int(e))
+print("10/2=",int(f))
+
+
+# In[ ]:
+
+
+
+
