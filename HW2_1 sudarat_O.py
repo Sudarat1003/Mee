4da956be40ba5489abafce3299b3c73e3dfb57f3
Sudarat1003 <52315113+Sudarat1003@users.noreply.github.com>
2019-07-22 18:46:05 +0700

    Add files via upload

---
+++ HW2_1 sudarat_O.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#นางสาว สุดารัต์  อ่อนดี 362515241017 EE36241N
+print("--------------Menu--------------")
+print("Welcome to Icecream Shop")
+print("1. Strawberry 80 THB")
+print("2. Vanila 50 THB")
+print("3. Chocolate 40 THB")
+print("4. Lemon 90 THB")
+print("5. Milk 10 THB")
+ice=int(input("What do you want?"))
+if ice==1 :
+    print("You order : 1 Strawberry 80 THB")
+elif ice==2 :
+    print("You order : 2 Vanila 50 THB")
+elif ice==3 :
+    print("You order : 3 Chocolate 40 THB")
+elif ice==4 :
+    print("You order : 4 Lemon 90 THB")
+elif ice==5 :
+    print("You order : 5 Milk 10 THB")
+
+
+# In[ ]:
+
+
+
+
